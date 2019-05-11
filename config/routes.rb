63766312d64ced5679d84c235a53ed335bb80112
NZTK <tkm.2438nishizawa@gmail.com>
2019-05-11 16:13:44 +0900

    [Add]table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :books
+  get 'books/show'
+  get 'books/index'
+  get 'books/new'
+  get 'books/edit'
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
